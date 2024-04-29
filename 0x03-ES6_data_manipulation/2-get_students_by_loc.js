30783e1a701e07bddfe147660ce7e6c5651610e0
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-04-29 17:39:43 +0000

    commitingfirstly

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++
@@ -1,5 +0,0 @@
-export default function getStudentsByLocation(array, city) {
-  return array.filter((i) => i.location === city);
-}export default function getStudentsByLocation(array, city) {
-  return array.filter((i) => i.location === city);
-}
---
+++ 0x03-ES6_data_manipulation/READEME.md
@@ -0,0 +1,1 @@
+this file is all about 0x03-ES6_data_manipulation
